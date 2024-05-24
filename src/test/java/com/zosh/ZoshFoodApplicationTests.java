b23e041084ed722b40bb1e23858f62e4abb549eb
SHOIB ANSARI <reddishshoib123@gmail.com>
2024-05-25 01:14:22 +0530

    Update ZoshFoodApplicationTests.java

--- src/test/java/com/zosh/ZoshFoodApplicationTests.java
+++ src/test/java/com/zosh/ZoshFoodApplicationTests.java
@@ -2,14 +2,14 @@ package com.zosh;
 
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.Assert.assertTrue;
+import org.junit.Assert;
 
 @SpringBootTest
 public class ZoshFoodApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		assertTrue(true); 
+		Assert.assertTrue(true);
 	}
 
 }
