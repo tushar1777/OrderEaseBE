a960d92bc0e5d07319c756a964f568cc63c4fb24
SHOIB ANSARI <reddishshoib123@gmail.com>
2024-05-24 17:21:31 +0530

    context added

--- src/main/java/com/zosh/ZoshFoodApplication.java
+++ src/main/java/com/zosh/ZoshFoodApplication.java
@@ -2,14 +2,15 @@ package com.zosh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ZoshFoodApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZoshFoodApplication.class, args);
-		Environment env = context.getEnvironment();
-		System.out.println("DB Username: " + env.getProperty("spring.datasource.username"));
-	}
-
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(ZoshFoodApplication.class, args);
+        Environment env = context.getEnvironment();
+        System.out.println("DB Username: " + env.getProperty("spring.datasource.username"));
+    }
 }
